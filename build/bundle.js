60f1a0fe095c4daf927bebbf68f17879bcb9e3ee
Jihoon Chae <hoonn94@naver.com>
2023-03-13 22:56:11 +0900

    feat : 웹팩설정 추가

---
+++ build/bundle.js
@@ -0,0 +1,244 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ \"./src/style.scss\");\n/* harmony import */ var _public_assets_images_beverage_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../public/assets/images/beverage.png */ \"./public/assets/images/beverage.png\");\n/* harmony import */ var _public_assets_images_chair_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../public/assets/images/chair.png */ \"./public/assets/images/chair.png\");\n/* harmony import */ var _public_assets_images_coffee_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../public/assets/images/coffee.png */ \"./public/assets/images/coffee.png\");\n/* harmony import */ var _public_assets_images_icecream_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../public/assets/images/icecream.png */ \"./public/assets/images/icecream.png\");\n/* harmony import */ var _public_assets_images_island_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../public/assets/images/island.png */ \"./public/assets/images/island.png\");\n/* harmony import */ var _public_assets_images_laptop_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../public/assets/images/laptop.png */ \"./public/assets/images/laptop.png\");\n/* harmony import */ var _public_assets_images_logo_cutout_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../public/assets/images/logo-cutout.png */ \"./public/assets/images/logo-cutout.png\");\n/* harmony import */ var _public_assets_images_money_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../public/assets/images/money.png */ \"./public/assets/images/money.png\");\n/* harmony import */ var _public_assets_images_people_png__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../public/assets/images/people.png */ \"./public/assets/images/people.png\");\n/* harmony import */ var _public_assets_images_plug_png__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ../public/assets/images/plug.png */ \"./public/assets/images/plug.png\");\n/* harmony import */ var _public_assets_images_short_cake_png__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ../public/assets/images/short-cake.png */ \"./public/assets/images/short-cake.png\");\n/* harmony import */ var _public_assets_images_sparkles_png__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ../public/assets/images/sparkles.png */ \"./public/assets/images/sparkles.png\");\n/* harmony import */ var _public_assets_images_speak_png__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ../public/assets/images/speak.png */ \"./public/assets/images/speak.png\");\n/* harmony import */ var _public_assets_images_osee_jpeg__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ../public/assets/images/osee.jpeg */ \"./public/assets/images/osee.jpeg\");\n/* harmony import */ var _public_assets_images_github_png__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! ../public/assets/images/github.png */ \"./public/assets/images/github.png\");\n/* harmony import */ var _public_assets_images_instagram_png__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! ../public/assets/images/instagram.png */ \"./public/assets/images/instagram.png\");\n// index.js\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n//# sourceURL=webpack://cafegolmok/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/style.scss":
+/*!************************!*\
+  !*** ./src/style.scss ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://cafegolmok/./src/style.scss?");
+
+/***/ }),
+
+/***/ "./public/assets/images/beverage.png":
+/*!*******************************************!*\
+  !*** ./public/assets/images/beverage.png ***!
+  \*******************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/beverage.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/beverage.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/chair.png":
+/*!****************************************!*\
+  !*** ./public/assets/images/chair.png ***!
+  \****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/chair.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/chair.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/coffee.png":
+/*!*****************************************!*\
+  !*** ./public/assets/images/coffee.png ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/coffee.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/coffee.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/github.png":
+/*!*****************************************!*\
+  !*** ./public/assets/images/github.png ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/github.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/github.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/icecream.png":
+/*!*******************************************!*\
+  !*** ./public/assets/images/icecream.png ***!
+  \*******************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/icecream.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/icecream.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/instagram.png":
+/*!********************************************!*\
+  !*** ./public/assets/images/instagram.png ***!
+  \********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/instagram.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/instagram.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/island.png":
+/*!*****************************************!*\
+  !*** ./public/assets/images/island.png ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/island.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/island.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/laptop.png":
+/*!*****************************************!*\
+  !*** ./public/assets/images/laptop.png ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/laptop.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/laptop.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/logo-cutout.png":
+/*!**********************************************!*\
+  !*** ./public/assets/images/logo-cutout.png ***!
+  \**********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/logo-cutout.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/logo-cutout.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/money.png":
+/*!****************************************!*\
+  !*** ./public/assets/images/money.png ***!
+  \****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/money.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/money.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/osee.jpeg":
+/*!****************************************!*\
+  !*** ./public/assets/images/osee.jpeg ***!
+  \****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/osee.jpeg\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/osee.jpeg?");
+
+/***/ }),
+
+/***/ "./public/assets/images/people.png":
+/*!*****************************************!*\
+  !*** ./public/assets/images/people.png ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/people.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/people.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/plug.png":
+/*!***************************************!*\
+  !*** ./public/assets/images/plug.png ***!
+  \***************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/plug.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/plug.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/short-cake.png":
+/*!*********************************************!*\
+  !*** ./public/assets/images/short-cake.png ***!
+  \*********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/short-cake.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/short-cake.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/sparkles.png":
+/*!*******************************************!*\
+  !*** ./public/assets/images/sparkles.png ***!
+  \*******************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/sparkles.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/sparkles.png?");
+
+/***/ }),
+
+/***/ "./public/assets/images/speak.png":
+/*!****************************************!*\
+  !*** ./public/assets/images/speak.png ***!
+  \****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/images/speak.png\";\n\n//# sourceURL=webpack://cafegolmok/./public/assets/images/speak.png?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		__webpack_require__.p = "/";
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
