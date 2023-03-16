b0e99f3771398bb7d3c9598fcf4352ebb5a93097
Jihoon Chae <hoonn94@naver.com>
2023-03-16 14:21:57 +0900

    feat : 홈컴포넌트 생성

---
+++ src/components/Home/Home.js
@@ -0,0 +1,65 @@
+// src/components/Home/Home.js
+
+import './Home.scss';
+
+const Home = () => {
+  const markup = `
+  <main>
+    <div class="posts-container">
+      <h2 class="screen-out">가고 싶은 카페를 클릭해 보세요</h2>
+      <ul class="posts">
+        <li class="post">
+          <article>
+            <img
+              src="assets/images/osee.jpeg"
+              alt="섬네일 이미지"
+              class="img-thumbnail"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              viewBox="0 0 24 24"
+              class="btn-bookmark"
+            >
+              <path
+                d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"
+              ></path>
+            </svg>
+            <dl class="tags">
+              <dt class="screen-out">카테고리</dt>
+              <dd class="tag-item">브런치</dd>
+              <dd class="tag-item">공간대여</dd>
+            </dl>
+
+            <div class="cafe-container">
+              <h3 class="cafe-name">오쎄</h3>
+              <dl>
+                <dt>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="15"
+                    viewBox="0 0 24 24"
+                    class="star"
+                  >
+                    <path
+                      d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"
+                    ></path>
+                  </svg>
+                </dt>
+                <dd class="star-number">4.43</dd>
+              </dl>
+            </div>
+            <strong class="cafe-location">사당역 도보 10분</strong>
+          </article>
+        </li>
+      </ul>
+    </div>
+  </main>
+
+  `;
+  return markup;
+};
+
+export default Home;
