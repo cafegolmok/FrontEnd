b1c22a614b3ddfa631a1fc38bb3e08a3b4dcd6b1
Jihoon Chae <hoonn94@naver.com>
2023-03-20 22:14:24 +0900

    feat : 브라우저 load및, 뒤로가기, 앞으로가기 클릭시 render 실행

--- src/components/app.js
+++ src/components/app.js
@@ -1,27 +1,36 @@
 // src/components/app.js
 
-// 애플리케이션의 논리를 구현하는 코드를 담고 있습니다. 즉, 애플리케이션의 구성 요소인 컴포넌트들을 정의하고, 라우팅 로직 등을 구현하는 역할을 합니다.
-import Header from './Header/Header';
-import Home from './Home/Home';
-import Footer from './Footer/Footer';
-import Login from './Login/Login';
 import Router from '../router';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import routes from '../routes'; 
 
-function App() {
+const App = () => {
   const router = new Router();
 
   const render = () => {
     const appElement = document.getElementById('app');
+
     appElement.innerHTML = `
       ${Header()}
-    ${router.getComponent()}
+      ${router.getComponent()()} 
       ${Footer()}
     `;
   };
 
-  return {
+  window.addEventListener('load', render);
+  window.addEventListener('popstate', render);
+  // router.init();
+
+  const app = {
     render
   };
-}
+
+  return app;
+};
 
 export default App;
+
+// 라우터 초기화
+// const router = new Router();
+// router.init();
