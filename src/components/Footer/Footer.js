f03ee9fa2466443fc8b74ef47b0d18455623c98f
Jihoon Chae <hoonn94@naver.com>
2023-03-23 19:43:43 +0900

    refactor : CafeList  컴포넌트를 DOM 요소를 생성하여 추가

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,47 @@
 import './Footer.scss';
 
 const Footer = () => {
-  const markup = `
-  <footer class="company-container">
-  <section class="company">
-    <h2 class="screen-out">기업정보</h2>
-    <strong class="title-company">카페골목</strong>
-    <ul class="link-sns">
-      <li>
-        <a href="#" class="link-github">
-          <img src="assets/images/github.png" alt="" />
-        </a>
-      </li>
+  const footer = document.createElement('footer');
+  footer.classList.add('company-container');
 
-      <li>
-        <a href="#" class="link-instagram">
-          <img src="assets/images/instagram.png" alt="" />
-        </a>
-      </li>
-    </ul>
-  </section>
-</footer>
-`;
-  return markup;
+  const section = document.createElement('section');
+  section.classList.add('company');
+
+  const title = document.createElement('strong');
+  title.classList.add('title-company');
+  title.textContent = '카페골목';
+
+  const ul = document.createElement('ul');
+  ul.classList.add('link-sns');
+
+  const li1 = document.createElement('li');
+  const a1 = document.createElement('a');
+  a1.href = '#';
+  a1.classList.add('link-github');
+  const img1 = document.createElement('img');
+  img1.src = 'assets/images/github.png';
+  img1.alt = 'GitHub 로고';
+  a1.appendChild(img1);
+  li1.appendChild(a1);
+
+  const li2 = document.createElement('li');
+  const a2 = document.createElement('a');
+  a2.href = '#';
+  a2.classList.add('link-instagram');
+  const img2 = document.createElement('img');
+  img2.src = 'assets/images/instagram.png';
+  img2.alt = 'Instagram 로고';
+  a2.appendChild(img2);
+  li2.appendChild(a2);
+
+  ul.appendChild(li1);
+  ul.appendChild(li2);
+
+  section.appendChild(title);
+  section.appendChild(ul);
+  footer.appendChild(section);
+
+  return footer;
 };
 
 export default Footer;
