c4ae3c6e9479cb99f8760d454155f41a8fd5e3a1
Jihoon Chae <hoonn94@naver.com>
2023-03-24 20:59:00 +0900

    rename : Error404 파일 위치변경

---
+++ src/pages/Error404.js
@@ -0,0 +1,10 @@
+// import './Error404.scss';
+
+const Error404 = () => {
+  const main = document.createElement('main');
+  main.textContent = 'Error 404';
+
+  return main;
+};
+
+export default Error404;
