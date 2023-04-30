2379ee45225c5a19fd3dbaae6ce55170d8b40fd0
Jihoon Chae <hoonn94@naver.com>
2023-04-30 22:02:09 +0900

    fix : 모달이 열릴때 오버레이가 생기면서 스크롤이 생기는 문제 해결

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,13 +1,25 @@
 // src/components/Overlay/Overlay.jsx
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { ModalOverlay } from './OverlayStyle';
 
 const Overlay = () => {
   const isLoginModalVisible = useSelector(state => state.isLoginModalVisible);
 
-  const isSignupModalVisible = useSelector(state => state.isSignupModalVisible)
+  const isSignupModalVisible = useSelector(state => state.isSignupModalVisible);
+
+  useEffect(() => {
+    if (isLoginModalVisible || isSignupModalVisible) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isLoginModalVisible, isSignupModalVisible]);
 
   if (!isLoginModalVisible && !isSignupModalVisible) {
     return null;
