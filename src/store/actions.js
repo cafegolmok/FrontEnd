85a5c57a26faab1a2f09e02f15ed373dd6c44d7d
Jihoon Chae <hoonn94@naver.com>
2023-05-16 14:41:34 +0900

    feat : 프로필 이미지 등록 리듀서와 액션 등록

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,22 +5,35 @@ export const SHOW_LOGIN_MODAL = 'SHOW_LOGIN_MODAL';
 export const HIDE_LOGIN_MODAL = 'HIDE_LOGIN_MODAL';
 export const SHOW_SIGNUP_MODAL = 'SHOW_SIGNUP_MODAL';
 export const HIDE_SIGNUP_MODAL = 'HIDE_SIGNUP_MODAL';
+export const SHOW_ADDPROFILEIMG_MODAL = 'SHOW_ADDPROFILEIMG_MODAL';
+export const HIED_ADDPROFILEIMG_MODAL = 'SHOW_ADDPROFILEIMG_MODAL';
 
-// 액션 생성자 함수: 모달 보이기
+// 로그인 모달 보이기
 export const showLoginModal = () => ({
   type: SHOW_LOGIN_MODAL,
 });
 
-// 액션 생성자 함수: 모달 숨기기
+// 로그인 모달 숨기기
 export const hideLoginModal = () => ({
   type: HIDE_LOGIN_MODAL,
 });
 
+// 회원가입 모달 보이기
 export const showSignupModal = () => ({
   type: SHOW_SIGNUP_MODAL,
 });
 
-
+// 회원가입 모달 숨기기
 export const hideSignupModal = () => ({
   type: HIDE_SIGNUP_MODAL,
+});
+
+// 프로필 이미지 등록 모달 보이기
+export const showAddProfileImgModal = () => ({
+  type: SHOW_ADDPROFILEIMG_MODAL,
+});
+
+// 프로필 이미지 등록 모달 숨기기
+export const hideAddProfileImgModal = () => ({
+  type: HIED_ADDPROFILEIMG_MODAL,
 });
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,12 +5,15 @@ import {
   HIDE_LOGIN_MODAL,
   SHOW_SIGNUP_MODAL,
   HIDE_SIGNUP_MODAL,
+  SHOW_ADDPROFILEIMG_MODAL,
+  HIED_ADDPROFILEIMG_MODAL,
 } from './actions';
 
 // 초기 상태 정의
 const initialState = {
   isLoginModalVisible: false,
   isSignupModalVisible: false,
+  isAddProfileImgModalVisible: true,
 };
 
 // 리듀서 함수: 상태와 액션을 받아 새로운 상태를 반환
@@ -28,6 +31,13 @@ const reducer = (state = initialState, action) => {
     // 회원가입 모달 숨기기 액션 처리
     case HIDE_SIGNUP_MODAL:
       return { ...state, isSignupModalVisible: false };
+
+    case SHOW_ADDPROFILEIMG_MODAL:
+      return { ...state, isAddProfileImgModalVisible: true };
+
+    case HIED_ADDPROFILEIMG_MODAL:
+      return { ...state, isAddProfileImgModalVisible: false };
+
     // 알 수 없는 액션은 현재 상태 반환
     default:
       return state;
