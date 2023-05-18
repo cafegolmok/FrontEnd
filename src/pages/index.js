3a8fdeb4fb0a1ded22fb97f32c1642eb140f2a98
Jihoon Chae <hoonn94@naver.com>
2023-05-18 21:54:50 +0900

    feat : pages폴더 기본 세팅

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+// src/pages/index.js
+
+import React from 'react';
+
+import CafeList from '../components/CafeList/CafeList.jsx';
+
+const Home = () => {
+  return (
+    <>
+      <CafeList />
+    </>
+  );
+};
+
+export default Home;
